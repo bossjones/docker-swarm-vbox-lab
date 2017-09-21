ce5e5a83fe58de894b8478ead63f8f5ed9ac3e18
Malcolm Jones <malcolm@adobe.com>
2017-09-20 23:51:38 -0400

    chg: test curl to each stat endpoint

--- scripts/create-p12-certs.sh
+++ scripts/create-p12-certs.sh
@@ -17,7 +17,7 @@ openssl pkcs12 -export -inkey key.pem -in cert.pem -CAfile ca.pem -chain -name c
 cd ~/.docker/machine/machines/node-03 && \
 openssl pkcs12 -export -inkey key.pem -in cert.pem -CAfile ca.pem -chain -name client-side  -out cert.p12 -password pass:mypass
 
-
+# Test curl to each stats endpoint for each host!
 _HOST=$(docker-machine ip swarm-manager)
 _DOCKER_CERT_PATH=~/.docker/machine/machines/swarm-manager
 curl https://$_HOST:2376/images/json --cert $_DOCKER_CERT_PATH/cert.p12 --pass mypass --key $_DOCKER_CERT_PATH/key.pem --cacert $_DOCKER_CERT_PATH/ca.pem
