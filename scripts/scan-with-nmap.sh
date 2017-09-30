52aa6eb8d242b73fb88a7555be1822d71dc2acf4
Malcolm Jones <malcolm@adobe.com>
2017-09-30 14:59:26 -0400

    Chg: nmap scan

--- scripts/scan-with-nmap.sh
+++ scripts/scan-with-nmap.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-nmap -v -A $(docker-machine ip swarm-manager)
-echo -e "----------------------------------------\n"
-nmap -v -A $(docker-machine ip node-01)
-echo -e "----------------------------------------\n"
-nmap -v -A $(docker-machine ip node-02)
-echo -e "----------------------------------------\n"
-nmap -v -A $(docker-machine ip node-03)
+echo -e "----------[swarm-manager]---------------\n"
+nmap -v $(docker-machine ip swarm-manager)
+echo -e "----------[node-01]---------------------\n"
+nmap -v $(docker-machine ip node-01)
+echo -e "----------[node-02]---------------------\n"
+nmap -v $(docker-machine ip node-02)
+echo -e "----------[node-03]---------------------\n"
+nmap -v $(docker-machine ip node-03)
