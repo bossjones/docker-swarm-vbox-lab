db386c226b27eb72379daf0299e8cac6b6c75fd4
Malcolm Jones <malcolm@adobe.com>
2017-09-29 22:53:58 -0400

    WIP: influx

--- scripts/create-influx-db.sh
+++ scripts/create-influx-db.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# docker exec `docker ps | grep -i influx | awk '{print $1}'` influx -execute 'CREATE DATABASE cadvisor'
+# source: https://docs.influxdata.com/influxdb/v1.3/guides/writing_data/
 
 _INFLUX_IP=$(docker-machine ip swarm-manager)
 
